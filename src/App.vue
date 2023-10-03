5326b28d9980e270d56043d392a19a243442f240
denisQa322 <denis.isakov.99@gmail.com>
2023-10-04 01:58:17 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+  </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            posts:[
+                {id:1, title: 'Javascript', body: 'Описание поста'},
+                {id:2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id:3, title: 'Javascript 3', body: 'Описание поста 3'},
+            ],
+            title:'',
+            body:''
+        }
+    },
+    methods:{
+        createPost(){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
+        },
+    }
+}
+</script>
+
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
+</style>
---
+++ src/Components/PostList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+        <div class="post" v-for="post in posts">
+        <div><strong>Название:</strong>{{ post.title}}</div>
+        <div><strong>Описание:</strong>{{ post.body}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.post{
+    padding: 15px;
+    border: 3px solid teal;
+    margin-top: 15px;
+}
+</style>
